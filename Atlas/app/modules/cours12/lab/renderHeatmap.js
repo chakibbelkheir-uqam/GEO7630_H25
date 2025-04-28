2f3dfb7f8a2e8ab1e56710a2c736246aa8fa5152
chakibbelkheir-uqam <belkheir.mohamed_chakib@courrier.uqam.ca>
2025-04-28 10:34:08 +0000

    lab 12

--- Atlas/app/modules/cours12/lab/renderHeatmap.js
+++ Atlas/app/modules/cours12/lab/renderHeatmap.js
@@ -10,7 +10,7 @@ function generateHeatmap() {
     map.addLayer({
         id: 'heatmap',
         source: 'heatmap-source',
-        type: _________,
+        type: 'heatmap',
         paint: {
             'heatmap-weight': ['get', 'point_count'],   // Poids des points basé sur 'point_count'
             'heatmap-radius': 20,                       // Rayon des points de chaleur du heatmap
@@ -31,5 +31,5 @@ function generateHeatmap() {
 }
 
 document
-  .getElementById(_________) // id unique du bouton
-  .addEventListener(__________, ______________); // ajoute un event de type click qui lance la fonction generateHeatmap()
+  .getElementById('generateHeatmap') // id unique du bouton
+  .addEventListener('click', generateHeatmap); // ajoute un event de type click qui lance la fonction generateHeatmap()
